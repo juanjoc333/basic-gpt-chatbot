f6fe82f95703e42af008319c87fe19c2c16a15d9
Juan Cortes <jcortes@fullstacklabs.com>
2023-10-17 21:38:34 -0600

    Adding some formatting and the default personality will be a pirate

--- chatbot.py
+++ chatbot.py
@@ -7,13 +7,25 @@ config = dotenv_values(".env")
 openai.api_key = config.get("OPENAI_API_KEY")
 
 
+def bold_red_text(text):
+    BOLD_RED = "\033[91;1m"  # 91 for red, 1 for bold
+    RESET = "\033[0m"
+    return BOLD_RED + text + RESET
+
+
+def bold_blue_text(text):
+    BOLD_BLUE = "\033[94;1m"  # 94 for blue, 1 for bold
+    RESET = "\033[0m"
+    return BOLD_BLUE + text + RESET
+
+
 def main():
     parser = argparse.ArgumentParser(description="Chat with GPT-3")
     parser.add_argument(
         "--personality",
         type=str,
         help="A brief summary of the personality of the chatbot",
-        default="A chatbot that can chat about anything",
+        default="a pirate",
     )
 
     args = parser.parse_args()
@@ -25,7 +37,7 @@ def main():
 
     while True:
         try:
-            user_input = input("You: ")
+            user_input = input(bold_blue_text("You: "))
             messages_list.append({"role": "user", "content": user_input})
 
             response = openai.ChatCompletion.create(
@@ -42,7 +54,7 @@ def main():
                 ],
             )
 
-            print("Assistant:", response.choices[0].message.content)
+            print(bold_red_text("Assistant: "), response.choices[0].message.content)
             messages_list.append(
                 {"role": "assistant", "content": response.choices[0].message.to_dict()}
             )
