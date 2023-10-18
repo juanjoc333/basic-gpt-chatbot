3b61c0184a1c2eac17eb79444f3847d149c4ef67
Juan Cortes <jcortes@fullstacklabs.com>
2023-10-17 21:22:28 -0600

    Adding personality to the chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,31 +1,56 @@
 import openai
 from dotenv import dotenv_values
 
+import argparse
+
 config = dotenv_values(".env")
 openai.api_key = config.get("OPENAI_API_KEY")
 
-messages_list = []
 
-while True:
-    try:
-        user_input = input("You: ")
-        messages_list.append({"role": "user", "content": user_input})
+def main():
+    parser = argparse.ArgumentParser(description="Chat with GPT-3")
+    parser.add_argument(
+        "--personality",
+        type=str,
+        help="A brief summary of the personality of the chatbot",
+        default="A chatbot that can chat about anything",
+    )
 
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "user",
-                    "content": user_input,
-                },
-            ],
-        )
+    args = parser.parse_args()
+    personality = args.personality
 
-        print("Assistant:", response.choices[0].message.content)
-        messages_list.append(
-            {"role": "assistant", "content": response.choices[0].message.to_dict()}
-        )
+    system_prompt = f"You are a conversational chatbot. You are a {personality}."
 
-    except KeyboardInterrupt:
-        print("Exiting...")
-        break
+    messages_list = []
+
+    while True:
+        try:
+            user_input = input("You: ")
+            messages_list.append({"role": "user", "content": user_input})
+
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": system_prompt,
+                    },
+                    {
+                        "role": "user",
+                        "content": user_input,
+                    },
+                ],
+            )
+
+            print("Assistant:", response.choices[0].message.content)
+            messages_list.append(
+                {"role": "assistant", "content": response.choices[0].message.to_dict()}
+            )
+
+        except KeyboardInterrupt:
+            print("Exiting...")
+            break
+
+
+if __name__ == "__main__":
+    main()
